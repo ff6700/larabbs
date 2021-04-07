fa70ad95699fc13f5b12ff836d9f229808294eb7
于少凡 <15734541240@163.com>
2021-04-07 22:25:50 +0800

    用户个人页面原型

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -12,4 +13,14 @@ class UsersController extends Controller
     {
         return view('users.show',compact('user'));
     }
+    public function edit(User $user)
+    {
+        return view('users.show',compact('user'));
+    }
+
+    public function update(UserRequest $request,User $user)
+    {
+        $user->update($request->all());
+    }
+
 }
